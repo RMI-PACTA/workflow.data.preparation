8ebf9a1a578926e6d74c0f8a0f66248adb75a22e
CJ Yetman <cyetman@rmi.org>
2023-03-20 21:39:03 +0100

    namespace currently unexpected function

--- run_pacta_data_preparation.R
+++ run_pacta_data_preparation.R
@@ -161,7 +161,7 @@ pacta.scenario.preparation::scenario_regions %>%
 
 
 log_info("Fetching currency data... ")
-get_currency_exchange_rates(
+pacta.data.preparation:::get_currency_exchange_rates(
   quarter = imf_quarter_timestamp
 ) %>%
   saveRDS(currencies_data_path)
