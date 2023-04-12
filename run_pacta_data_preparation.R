c263d0b8bdbbf53dbde21626155b125c55d31636
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-04-12 14:26:07 +0200

    Add `tdm_years` concept to data preparation (#34)
    This PR:
    * Adds a new parameter, `tdm_delta_years`, to the `config.yml`, which
    indicates any extra years that scenario targets need be calculated for,
    to be used in the TDM. These years are "deltas" meaning a duration, as
    measured from the start year (rather than a fixed value)
    * Adds these new years to the `run_pacta_data_preparation` script
    
    What this PR does NOT do: Test if those years are even present in any of
    the scenario data!!
    
    Relates to
    https://github.com/RMI-PACTA/pacta.portfolio.analysis/issues/219
    
    Closes #31
    
    ---------
    
    Co-authored-by: CJ Yetman <cyetman@rmi.org>

--- run_pacta_data_preparation.R
+++ run_pacta_data_preparation.R
@@ -90,6 +90,7 @@ relevant_years <- sort(
   unique(
     c(
       market_share_target_reference_year:(market_share_target_reference_year + time_horizon),
+      market_share_target_reference_year + tdm_delta_years,
       additional_year
     )
   )
@@ -290,7 +291,7 @@ scenario_regions <- readr::read_csv(scenario_regions_path, na = "", show_col_typ
 
 index_regions <- pacta.data.scraping::get_index_regions()
 
-factset_issue_code_bridge <- 
+factset_issue_code_bridge <-
   pacta.data.preparation::factset_issue_code_bridge %>%
   select(issue_type_code, asset_type) %>%
   mutate(
@@ -303,7 +304,7 @@ factset_issue_code_bridge <-
     )
   )
 
-factset_industry_map_bridge <- 
+factset_industry_map_bridge <-
   pacta.data.preparation::factset_industry_map_bridge %>%
   select(factset_industry_code, pacta_sector)
 
