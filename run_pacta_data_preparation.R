fdc6f91afb191a00067740b3809aec10fdddf53b
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-05-08 14:36:35 +0200

    Remove unnecessary .data pronoun (#52)
    join needs a named vector, not a `.data$` pronoun.

--- run_pacta_data_preparation.R
+++ run_pacta_data_preparation.R
@@ -262,8 +262,8 @@ scenarios_long <- scenario_raw %>%
   inner_join(
     pacta.scenario.preparation::scenario_source_pacta_geography_bridge,
     by = c(
-      .data$scenario_source = "source",
-      .data$scenario_geography = "scenario_geography_source"
+      scenario_source = "source",
+      scenario_geography = "scenario_geography_source"
       )
     ) %>%
   select(-.data$scenario_geography) %>%
