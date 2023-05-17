RMI-PACTA/workflow.data.preparation

commit c9a6246ea5e8adacc346e0865190e9d54e237bdf

Remove deprecated use of `.data$` in tidyselect functions (#53)
This will always confuse me. But tidyselect prefers quoted names over
the `.data` pronoun.

run_pacta_data_preparation.R
@@ -266,7 +266,7 @@ scenarios_long <- scenario_raw %>%
       scenario_geography = "scenario_geography_source"
       )
     ) %>%
-  select(-.data$scenario_geography) %>%
+  select("scenario_geography") %>%
   rename(scenario_geography = "scenario_geography_pacta") %>%
   filter(
     .data$scenario_source %in% .env$scenario_sources_list,

commit 3db1a67ad4d66e78054a895db9cd03a1953f1be4

Add missing `-` sign to call to `select()` (#55)
Fixes a bug that I introduced in
https://github.com/RMI-PACTA/workflow.data.preparation/pull/53

run_pacta_data_preparation.R
@@ -266,7 +266,7 @@ scenarios_long <- scenario_raw %>%
       scenario_geography = "scenario_geography_source"
       )
     ) %>%
-  select("scenario_geography") %>%
+  select(-"scenario_geography") %>%
   rename(scenario_geography = "scenario_geography_pacta") %>%
   filter(
     .data$scenario_source %in% .env$scenario_sources_list,